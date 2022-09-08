1d3a912537696f7cae4b869284cfecf8a6f07db0
ManHoff <achoffman91@gmail.com>
2022-09-07 23:09:47 -0400

    added comments and finished question prompts

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ var charCap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", "?", "=", "_"]
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var output = [];
-var options = [];
+var options = [];    //empty arrays for pushing user input values into
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,39 +15,39 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
 
 
-  function generatePassword() {
+  function generatePassword() {                                     //setting variable for users desired input through window prompt question
     var chooseCharAmount = window.prompt("How many characters would you like your password to be?\nChoose a number between 8 and 128.");
 
     if (chooseCharAmount < 8 || chooseCharAmount > 128) {         //checking user input to see if its within the acceptable range
       window.alert("Number of characters is out of range, try again.");
       
 
-    } else if (chooseCharAmount >= 8 && chooseCharAmount <= 128) {
+    } else if (chooseCharAmount >= 8 && chooseCharAmount <= 128) {    
    
-    } 
-    var chooseCap = confirm("Would you like to include capital letters?");
-    if (chooseCap) {
-      output.push(charCap[Math.floor(Math.random() * charCap.length)]);
+    }                                                            //user input prompt for capitol letters
+    var chooseCap = confirm("Would you like to include capital letters?");   
+    if (chooseCap) {        //if this value is true capitol letters will be included in users password
+      output.push(charCap[Math.floor(Math.random() * charCap.length)]);      //pushing into arrays and randomizing array content for password character bank
       options.push(charCap);
-      // console.log(output);
-      // console.log(options);
+    
 
-    }
+    }                                                              // //user input prompt for lowercase letters
     var chooseLow = confirm("Would you like your password to include lowercase letters?");
-    if (chooseLow) {
+    if (chooseLow) {           //if this value is true lowercase letters will be included in users password
       output.push(charLower[Math.floor(Math.random() * charLower.length)]);
       options.push(charLower);
-    //   console.log(output);
-    //   console.log(charCap);
-      }
-    // var chooseSpecial = confirm("Would you like your password to include special characters?");
-    // if (chooseSpecial) {
-    //   output += specialChar;
-    // }
-    // var chooseNum = confirm("Would you like your password to include numbers in your password?");
-    // if (chooseNum) {
-    //   output += numbers;
-    // }
+  
+      }                                                             //user input prompt for special characters
+    var chooseSpecial = confirm("Would you like your password to include special characters?");
+    if (chooseSpecial) {         //if this value is true will be included in users password
+      output.push(specialChar[Math.floor(Math.random() * specialChar.length)]);
+      options.push(specialChar)
+    }                                                                //user input prompt for numbers
+    var chooseNum = confirm("Would you like your password to include numbers in your password?");
+    if (chooseNum) {
+      output.push(numbers[Math.floor(Math.random() * numbers.length)]);
+      options.push(numbers);
+    }
   
 
     var remainingChoices = chooseCharAmount - output.length;
